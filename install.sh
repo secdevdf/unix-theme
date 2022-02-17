f68e06839d29966550fefbba7c5eb124e958c55b
Gabriel Machado @DevLaw <97376083+secdevdf@users.noreply.github.com>
2022-02-17 10:29:58 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+tar -xvf retro-theme.tar.xz
+tar -xvf Lyraq-Cursors.tar.gz
+tar -xvf Fancy-Dark-Icons.tar.gz
+tar -xvf Diolinux\ Terminal\ Colour.tar.gz
+
+mv retro-theme/ /usr/share/themes/
+mv Lyraq-Cursors/ /usr/share/icons/
+mv Fancy-Dark-Icons/ /usr/share/icons/
+mv .bashrc /home/*/.bashrc 
+                          
