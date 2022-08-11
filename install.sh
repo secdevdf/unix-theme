b8346377f3a977a2d81320dafe5ae0f9607779de
Gabriel Machado @DevLaw <97376083+secdevdf@users.noreply.github.com>
2022-08-11 20:58:38 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ tar -xvf Fancy-Dark-Icons.tar.gz
 tar -xvf Diolinux\ Terminal\ Colour.tar.gz
 
 mv retro-theme/ /usr/share/themes/
-mv Lyraq-Cursors/ /usr/share/icons/
+mv LyraQ-cursors/ /usr/share/icons/
 mv Fancy-Dark-Icons/ /usr/share/icons/
-mv .bashrc /home/*/.bashrc 
-                          
+cp .bashrc /home/*/.bashrc
+mv .bashrc /root/.bashrc
